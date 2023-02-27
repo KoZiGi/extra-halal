cfc51ee231ffaf28f9cba3f606eeebde9a08365b
zickbalazs <zickbalazs@turr.hu>
2023-02-27 11:21:47 +0100

    init commit

---
+++ api/adatok.php
@@ -0,0 +1,9 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = '2123szft_pizzeria';
+    $apiver = 'v2.230202';
+    $apiname = 'BSZC Türr Simple PHP Backend API';
+    $uploadDir = 'uploads/';
+?>
