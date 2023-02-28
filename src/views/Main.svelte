eab58fbb634877dbc8c0b9611d0915a04c29774b
ginalzsolt <ginalzsolt@turr.hu>
2023-02-28 13:49:13 +0100

    hr

--- src/views/Main.svelte
+++ src/views/Main.svelte
@@ -10,6 +10,13 @@
         <h2 class="text-muted">Üdvözöllek Barátok!</h2>
     </header>
     <section>
+        <p>Megtaláltad a világ legjobb Gyrososát az Extra-halalt! Nálunk igazi török gyrost ehetsz, amihez haszonlót sehol máshol nem találsz széles e világon! Séfeink képzett forgatói a gyroskardnak és az igazi Türk Kebab élményt nyújtják vendégeinknek. Minden betérő vendég edig soha nem tapasztalt élmennyel hagyja el éttermünket, teli hassal jókedvvel és az igazi török gyros adta eufóriával.</p>
+        <p>Mesterszkácsaink Ahmet, Mustafa, Selim és Kahraman évek óta készítenek gyrost, minden reggel saját kezűleg vadásszák le az aznap felszolgált gyroshoz a birkákat. Ezeket a birkákat egy szabad legelőn tartjuk és neveljük így húsuk kimagasló minőséget képvisel és semmihez sem fogható ízt ad.</p>
+        <p>Eleinte egy kis bódéból árultuk csak ételeinket de napjanikra már mi vagyunk a világ egyik legnagyobb Gyros gyorsétterme. Cégünk minden dolgozóra családtagként tekint és segíti a bajban, nélkülük nem lehetne az Extra-Halal az az étterem ahogy mára már mindenki ismeri és szereti.</p>
+        <p>Ha te is kíváncsi vagy milyen az igazi türkishe kebab rendelj tőlönk! Futáraink 1 órán bellül kiérnek bárhol is vagy, megtalálunk.</p>
+        
+        <hr>
+        
         <p>
             Dünyanın en iyi Gyrososa'sını buldunuz, Ekstra Helal! Dünyanın hiçbir yerinde bulamayacağınız gerçek Türk dönerlerini burada yiyebilirsiniz! Aşçılarımızın hünerli eğiricileri, misafirlerimize jiroskard ve gerçek Türk Kebap deneyimini yaşatıyor. Gelen her misafirimiz, daha önce hiç tatmadığı bir deneyimle, tok karnına ve gerçek Türk cayrolarının coşkusuyla restoranımızdan ayrılıyor.
         </p>
@@ -22,9 +29,5 @@
         <p>
             Siz de gerçek Türk kebabının nasıl olduğunu merak ediyorsanız bizden sipariş verin! Kuryelerimiz 1 saat içinde gelecek, nerede olursanız olun sizi bulacağız.
         </p>
-        <p>Megtaláltad a világ legjobb Gyrososát az Extra-halalt! Nálunk igazi török gyrost ehetsz, amihez haszonlót sehol máshol nem találsz széles e világon! Séfeink képzett forgatói a gyroskardnak és az igazi Türk Kebab élményt nyújtják vendégeinknek. Minden betérő vendég edig soha nem tapasztalt élmennyel hagyja el éttermünket, teli hassal jókedvvel és az igazi török gyros adta eufóriával.</p>
-        <p>Mesterszkácsaink Ahmet, Mustafa, Selim és Kahraman évek óta készítenek gyrost, minden reggel saját kezűleg vadásszák le az aznap felszolgált gyroshoz a birkákat. Ezeket a birkákat egy szabad legelőn tartjuk és neveljük így húsuk kimagasló minőséget képvisel és semmihez sem fogható ízt ad.</p>
-        <p>Eleinte egy kis bódéból árultuk csak ételeinket de napjanikra már mi vagyunk a világ egyik legnagyobb Gyros gyorsétterme. Cégünk minden dolgozóra családtagként tekint és segíti a bajban, nélkülük nem lehetne az Extra-Halal az az étterem ahogy mára már mindenki ismeri és szereti.</p>
-        <p>Ha te is kíváncsi vagy milyen az igazi türkishe kebab rendelj tőlönk! Futáraink 1 órán bellül kiérnek bárhol is vagy, megtalálunk.</p>
     </section>
 </div>
