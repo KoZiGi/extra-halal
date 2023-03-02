3ebedcaf35814d5d86d1d42f2759d865f6904dfd
ginalzsolt <ginalzsolt@turr.hu>
2023-03-02 11:13:47 +0100

    Card modul

--- src/views/Gyros.svelte
+++ src/views/Gyros.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { each } from "svelte/internal";
+  import GyrosCard from "./GyrosCard.svelte";
   import type { Gyros } from "../interfaces/gyros";
   let gyrosok: Gyros[];
 </script>
@@ -8,34 +9,7 @@
   <h2>Extra-Halal ételek (No Haram)</h2>
   <div id="menu" class="d-flex flex-wrap">
     {#each gyrosok as gyros}
-      <div class="card" style="width: 18rem;">
-        <div class="card-header">
-          <h5 class="card-title">{gyros.name}</h5>
-        </div>
-        <img src={gyros.image} class="card-img-top" alt="..." />
-        <div class="card-body">
-          <div class="flex" />
-          <p class="card-text">{gyros.description}</p>
-        </div>
-        <div class="card-footer">
-          <div class="input-group mb-3">
-            <input
-              type="number"
-              class="form-control"
-              placeholder="Recipient's username"
-              aria-label="Recipient's username"
-              aria-describedby="button-addon2"
-              value="0"
-              min="0"
-            />
-            <button
-              class="btn btn-outline-secondary"
-              type="button"
-              id="button-addon2">Rendel</button
-            >
-          </div>
-        </div>
-      </div>
+      <GyrosCard gyros={gyros}/>
     {/each}
   </div>
 </div>
---
+++ src/views/GyrosCard.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import type { Gyros } from "../interfaces/gyros";
+    export let gyros:Gyros
+    let gyrosamount:number=0
+    function Order(){
+        alert("bruh")
+    }
+
+</script>
+
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+      <h5 class="card-title">{gyros.name}</h5>
+    </div>
+    <img src={gyros.image} class="card-img-top" alt="..." />
+    <div class="card-body">
+      <div class="flex" />
+      <p class="card-text">{gyros.description}</p>
+    </div>
+    <div class="card-footer">
+      <div class="input-group mb-3">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="0"
+          bind:value={gyrosamount}
+          min="0"
+        />
+        <button
+          class="btn btn-outline-secondary"
+          type="button"
+          on:click={()=>{Order()}}
+          >
+          Rendel
+          </button>
+      </div>
+    </div>
+  </div>
