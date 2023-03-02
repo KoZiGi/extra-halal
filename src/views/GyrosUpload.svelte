bec829dc9283648b365e5dfdd414c158383585d5
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2023-03-02 11:07:55 +0100

    gyros upload form kész

--- src/views/GyrosUpload.svelte
+++ src/views/GyrosUpload.svelte
@@ -1,51 +1,35 @@
 <script lang="ts">
-    let name ="";
-    let cost=0;
-    let des="";
-    let file=null;
+    let gyros={
+        name:"",
+        cost:0,
+        des:"",
+        file:null
+    }
     
 </script>
 
 <main>
-    <div id="mydiv">
-        <div id="forminputs">
-            <div class="input-group flex-nowrap d-flex flex-column formelement">
-                <label for="">isim <sup>(name)</sup>: </label>
-                <input type="text" class="form-control w-75" bind:value={name}>
-            </div>
-            <div class="input-group flex-nowrap d-flex flex-column formelement">
-                <label for="">fiyat<sup>(cost)</sup>: </label>
-                <input type="text" class="form-control w-75" bind:value={cost}>
-            </div>
-            <div class="input-group flex-nowrap d-flex flex-column formelement">
-                <label for="">Tanim<sup>(description)</sup>: </label>
-                <input type="text" class="form-control w-75" bind:value={des}>
-            </div>
-            <div class="input-group flex-nowrap d-flex flex-column formelement">
-                <label for="">resim<sup>(picture)</sup>: </label>
-                <input type="text" class="form-control w-75" bind:value={file}>
-            </div>
-            <button type="button" class="btn btn-primary">Submit</button>
+    <div id="login" class="col-lg-7 col-md-9 col-11 border border-dark rounded p-3 mx-auto">
+        <h2>Gyros feltöltés: </h2>
+        <div class="mb-3">
+            <label for="name" class="form-label">isim (name)</label>
+            <input type="text" id="name" class="form-control" bind:value={gyros.name} />
         </div>
+        <div class="mb-3">
+            <label for="cost" class="form-label">fiyat (cost)</label>
+            <input type="number" id="cost" class="form-control" bind:value={gyros.cost} />
+        </div>
+        <div class="mb-3">
+            <label for="des" class="form-label">tanim (description)</label>
+            <textarea id="des" class="form-control" bind:value={gyros.des} />
+        </div>
+        <div class="mb-3">
+            <label for="pic" class="form-label">resim (picture)</label>
+            <input type="file" id="pic" class="form-control " bind:value={gyros.file}>
+        </div>
+        <button class="btn btn-primary">Upload</button>
     </div>
 </main>
 
 <style lang="sass">
-    main
-        height: 90%
-        width: 100%        
-    #mydiv
-        height: 90%
-        width: 100%
-    .formelement
-        padding-left: 21.5%
-    #forminputs
-        width: 50vw
-        border-radius: 2%
-        border: black 1px solid
-        margin: auto
-    button
-        margin-top: 3%
-        margin-left: 40%
-        margin-bottom: 3% 
 </style>
