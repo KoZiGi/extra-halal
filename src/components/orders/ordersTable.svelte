1b4bb405738fb964d84ec6ed25e770724b0f138b
zickbalazs <zickbalazs@turr.hu>
2023-03-02 13:42:28 +0100

    Orders javítva

--- src/components/orders/ordersTable.svelte
+++ src/components/orders/ordersTable.svelte
@@ -1,51 +1,59 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
     import type { Order } from "../../interfaces/orders";
-    import { DeleteOrderData, GetOrders, MarkOrderAsDone } from "../../services/orderService";
+    import {
+        DeleteOrderData,
+        GetOrders,
+        MarkOrderAsDone,
+    } from "../../services/orderService";
     let AllOrders = GetOrders();
     const dispatch = createEventDispatcher();
-    function DeleteOrder(orderID:number){
-        DeleteOrderData(orderID).then(()=>{
+    function DeleteOrder(orderID: number) {
+        DeleteOrderData(orderID).then(() => {
             AllOrders = GetOrders();
-        })
-    };
-    function MarkOrder(orderID:number){
-        MarkOrderAsDone(orderID).then(()=>{
+        });
+    }
+    function MarkOrder(orderID: number) {
+        MarkOrderAsDone(orderID).then(() => {
             AllOrders = GetOrders();
-        })
-    };
-    function ViewOrder(order:Order){
-        dispatch('order', {order:order});
+        });
+    }
+    function ViewOrder(order: Order) {
+        dispatch("order", { order: order });
     }
 </script>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>#.</th>
-            <th>Ameliyat</th>
-            <th>Adres</th>
-            <th>Toplam tutar</th>
-            <th>Operasyonlar</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#await AllOrders}
-            <div class="spinner-border"></div>
-        {:then Data} 
-            {#each Data as order}
-                <tr class={order.isDone == true ? "table-success" : ""}>
-                    <td>{order.ID}</td>
-                    <td>{order.username}{#if order.isDone}<i class="bi bi-check-lg"></i>{/if}</td>
-                    <td>{order.address}</td>
-                    <td>{new Intl.NumberFormat('hu-HU', {style:'currency', currency:'HUF'}).format(Array.from(new Set(order.items.map(g=>{return g.price*g.amount}))).reduce((a,b)=>a+b))}</td>
-                    <td>
-                        <button class="btn btn-primary" title="siparişi görüntüle" on:click={()=>{ViewOrder(order)}} data-bs-toggle="modal" data-bs-target="#ordermodal"><i class="bi bi-eye"></i></button>
-                        <button class="btn btn-success" title="tamamlandı olarak işaretle" on:click={()=>{MarkOrder(order.ID)}}><i class="bi bi-check-lg"></i></button>
-                        <button class="btn btn-danger" title="siparişi sil" on:click={()=>{DeleteOrder(order.ID)}}><i class="bi bi-trash"></i></button>
-                    </td>
+{#await AllOrders}
+    <div class="spinner-border" />
+{:then Data}
+    {#if Data.length > 0}
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#.</th>
+                    <th>Ameliyat</th>
+                    <th>Adres</th>
+                    <th>Toplam tutar</th>
+                    <th>Operasyonlar</th>
                 </tr>
-            {/each}
-        {/await}
-    </tbody>
-</table>
+            </thead>
+            <tbody>
+                {#each Data as order}
+                    <tr class={order.isDone == true ? "table-success" : ""}>
+                        <td>{order.ID}</td>
+                        <td>{order.username}{#if order.isDone}<i class="bi bi-check-lg"/>{/if}</td>
+                        <td>{order.address}</td>
+                        <td>{new Intl.NumberFormat("hu-HU", {style: "currency",currency: "HUF",}).format(Array.from(new Set(order.items.map((g) => {return g.price * g.amount;}))).reduce((a, b) => a + b))}</td>
+                        <td>
+                            <button class="btn btn-primary" title="siparişi görüntüle" on:click={() => {ViewOrder(order);}} data-bs-toggle="modal" data-bs-target="#ordermodal"><i class="bi bi-eye" /></button>
+                            <button class="btn btn-success" title="tamamlandı olarak işaretle" on:click={() => { MarkOrder(order.ID); }}><i class="bi bi-check-lg" /></button>
+                            <button class="btn btn-danger" title="siparişi sil" on:click={() => { DeleteOrder(order.ID); }}><i class="bi bi-trash" /></button>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    {:else}
+        <h2 class="text-danger display-1 fw-b">Henüz sipariş yok!</h2>
+    {/if}
+{/await}
