f437f2f65f90990cab445d3a93f3b8f8ec11dc74
ginalzsolt <ginalzsolt@turr.hu>
2023-03-02 13:37:57 +0100

    cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
     import Routing from "./routing.svelte";
     import Navbar from "./components/Navbar.svelte";
-    import GyrosUpload from "./views/GyrosUpload.svelte";
-
 </script>
 
 <style>
     
 </style>
 <Navbar/>
-<GyrosUpload/>
 <Routing/>
--- src/views/GyrosUpload.svelte
+++ src/views/GyrosUpload.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
     import type {Gyros} from '../interfaces/gyros';
-    import axios from 'axios';
     import { PostGyros } from '../services/gyrosService';
-    let object={
-        values:{
+    let files:FileList
+   
+    let values:Gyros={
             name:"",
             price:0,
             kcal:0,
             description:"",
             image: ""
-        },
-        table: "gyrosok"
-    } 
-
-    
-    
+        }
+    async function Up(){
+        values.image=files[0].name
+        console.log(files)
+        console.log(values)
+        await PostGyros(values)
+    }
 </script>
 
 <main>
@@ -22,25 +23,25 @@
         <h2>Gyros feltöltés: </h2>
         <div class="mb-3">
             <label for="name" class="form-label">isim (name)</label>
-            <input type="text" id="name" class="form-control" bind:value={object.values.name} />
+            <input type="text" id="name" class="form-control" bind:value={values.name} />
         </div>
         <div class="mb-3">
             <label for="cost" class="form-label">fiyat (cost)</label>
-            <input type="number" id="cost" class="form-control" bind:value={object.values.price} />
+            <input type="number" id="cost" class="form-control" bind:value={values.price} />
         </div>
         <div class="mb-3">
             <label for="kcal" class="form-label">kalori (kcal)</label>
-            <input type="number" id="kcal" class="form-control" bind:value={object.values.kcal} />
+            <input type="number" id="kcal" class="form-control" bind:value={values.kcal} />
         </div>
         <div class="mb-3">
             <label for="des" class="form-label">tanim (description)</label>
-            <textarea id="des" class="form-control" bind:value={object.values.description} />
+            <textarea id="des" class="form-control" bind:value={values.description} />
         </div>
         <div class="mb-3">
             <label for="pic" class="form-label">resim (picture)</label>
-            <input type="file" id="pic" class="form-control " bind:value={object.values.image}>
+            <input type="file" id="pic" class="form-control " bind:files={files}>
         </div>
-        <button type="button" class="btn btn-primary" on:click={()=>PostGyros(object)}>Upload</button>
+        <button type="button" class="btn btn-primary" on:click={()=>{Up()}}>Upload</button>
     </div>
 </main>
 
